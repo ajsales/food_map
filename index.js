80d08c22d2d7ac49ccb426265093a2deb9b412e7
Adrian Jules Sales <ajsales@berkeley.edu>
2021-05-07 00:23:53 -0700

    Added comments, made code prettier

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map;
 let service;
 let infoWindow;
 
-
+// Initializes map to either Northern California or current location
 function initMap() {
 	
 	map = new google.maps.Map(document.getElementById("map"), {
@@ -13,10 +13,11 @@ function initMap() {
 	
 
 	// Try HTML5 geolocation.
-	// geolocation();
+	geolocation();
 
 }
 
+// Moves center of the map to user's current location
 function geolocation() {
 	infoWindow = new google.maps.InfoWindow();
 	if (navigator.geolocation) {
@@ -42,6 +43,7 @@ function geolocation() {
 	}
 }
 
+// Handles error if browser doesn't support geolocation
 function handleLocationError(browserhasGeolocation, infoWindow, pos) {
 	infoWindow.setPosition(pos);
 	infoWindow.setContent(
@@ -52,27 +54,38 @@ function handleLocationError(browserhasGeolocation, infoWindow, pos) {
 	infoWindow.open(map);
 }
 
+// Creates a marker given the location and icon
 function createMarker(place, icon) {
+
+	// Scaled icon
 	const image = {
 		url: icon,
 		scaledSize: new google.maps.Size(30, 30)
 	};
+
+	// Google Maps Marker object
 	const marker = new google.maps.Marker({
 		map,
 		position: place.geometry.location,
 		icon: image,
 	});
+
+	// Markers can be clicked to show the name of the place
 	infoWindow = new google.maps.InfoWindow();
 	google.maps.event.addListener(marker, "click", () => {
 		infoWindow.setPosition(place.geometry.location);
     	infoWindow.setContent(place.name);
     	infoWindow.open(map);
   	});
+
   	return marker;
 }
 
+// Returns array of markers given a search query
 function addMarkerFromQuery(query, icon) {
-	var request = {
+
+	// The request is made through Google Places
+	const request = {
 	    query: query,
 	    fields: ["name", "geometry"],
 	    location: map.getCenter(),
@@ -80,18 +93,24 @@ function addMarkerFromQuery(query, icon) {
 
 	service = new google.maps.places.PlacesService(map);
 
+	// A marker is made for each valid search result
 	let markers = [];
 	service.textSearch(request, (results, status) => {
 		if (status === google.maps.places.PlacesServiceStatus.OK) {
-	    	for (var i = 0; i < results.length; i++) {
-	        	markers.push(createMarker(results[i], icon));
+	    	for (let i = 0; i < results.length; i++) {
+	    		let marker = createMarker(results[i], icon);
+	        	markers.push(marker);
 	    	}
+
+	    	// Map is re-centered to the first search result
 	    	map.setCenter(results[0].geometry.location);
 		}
 	});
+
 	return markers;
 }
 
+// The categories of food places to be queried
 const places = [
 	{
 		query: "Brazilian steakhouse",
@@ -111,49 +130,63 @@ const places = [
 	}
 ]
 
-let curr_markers = [];
+let currMarkers = [];
 for (let i = 0; i < places.length; i++) {
-	curr_markers.push([]);
+	currMarkers.push([]);
 }
 
+// Button behavior for the query page buttons
 const choiceList = document.querySelectorAll(".choice");
 for (let i = 0; i < choiceList.length; i++) {
 	const choiceEl = choiceList[i];
-	choiceEl.addEventListener("click", () => {
+
+	choiceEl.addEventListener("click", (e) => {
+		e.preventDefault();
+
 		choiceEl.classList.toggle("active-choice");
+
+		// Adds/removes markers if button is active or not
 		if (choiceEl.classList.contains("active-choice")) {
 			const markers = addMarkerFromQuery(places[i].query, places[i].icon);
-			curr_markers[i] = markers;
+			currMarkers[i] = markers;
 		} else {
-			curr_markers[i].forEach(marker => {
-				marker.setMap(null);
-			})
-			curr_markers[i] = [];
+			currMarkers[i].forEach(marker => marker.setMap(null));
+			currMarkers[i] = [];
 		}
 	})
 }
 
-const navbar_containers = document.querySelectorAll(".navbar-container");
+// Tab behavior to switch between the query and credits pages
+const navbarContainers = document.querySelectorAll(".navbar-container");
 const contents = document.querySelectorAll(".content");
-navbar_containers.forEach(navbar_container => {
-	navbar_container.addEventListener("click", e => {
-		if (!navbar_container.classList.contains("navbar-active")) {
+navbarContainers.forEach(navbarContainer => {
+
+	navbarContainer.addEventListener("click", e => {
+		e.preventDefault();
+
+		// Changes active tab and removes its hover behavior
+		if (!navbarContainer.classList.contains("navbar-active")) {
+
 			if (e.target.firstElementChild) {
 				e.target.firstElementChild.classList.remove("navbar-hover");
 			} else {
 				e.target.classList.remove("navbar-hover");
 			}
-			navbar_containers.forEach(x => x.classList.toggle("navbar-active"));
+
+			navbarContainers.forEach(x => x.classList.toggle("navbar-active"));
 			contents.forEach(x => x.classList.toggle("hidden"));
 		}
 	})
-	navbar_container.addEventListener("mouseenter", e => {
-		if (!navbar_container.classList.contains("navbar-active")) {
+
+	// Gives hover behavior to non-active tab
+	navbarContainer.addEventListener("mouseenter", e => {
+		if (!navbarContainer.classList.contains("navbar-active")) {
 			e.target.firstElementChild.classList.add("navbar-hover");
 		}
 	})
-	navbar_container.addEventListener("mouseleave", e => {
-		if (!navbar_container.classList.contains("navbar-active")) {
+
+	navbarContainer.addEventListener("mouseleave", e => {
+		if (!navbarContainer.classList.contains("navbar-active")) {
 			e.target.firstElementChild.classList.remove("navbar-hover");
 		}
 	})
